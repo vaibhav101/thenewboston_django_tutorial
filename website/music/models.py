425f6d486bfc9dd97d3f6ae95e379792d51cf0a2
Vaibhav Paliwal <vaibhav.paliwal@outlook.com>
2018-08-07 02:00:34 +0530

    - Added __str__ methods for models
    - Registered the Album model in admin.py

--- website/music/models.py
+++ website/music/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Album(models.Model):
+    def __str__(self):
+        return "%s - %s" % (self.artist, self.album_title)
+
     artist = models.CharField(max_length=200)
     album_title = models.CharField(max_length=200)
     genre = models.CharField(max_length=200)
@@ -9,6 +12,9 @@ class Album(models.Model):
 
 
 class Song(models.Model):
+    def __str__(self):
+        return "%s - %s" % (self.album, self.song_title)
+
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=200)
