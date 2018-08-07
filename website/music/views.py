50ec04d73b4c029e58041606a460ab0a3b0f7ee8
Vaibhav Paliwal <vaibhav.paliwal@outlook.com>
2018-08-07 15:13:05 +0530

    - Went from using django.template.loader to using django.shortcuts.render

--- website/music/views.py
+++ website/music/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
 from .models import Album
-from django.template import loader
+from django.shortcuts import render
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
     context = {
         'all_albums' : all_albums,
     }
-    return HttpResponse(template.render(context, request))
+
+    return render(request, 'music/index.html', context=context)
 
 
 def detail(request, album_id):
