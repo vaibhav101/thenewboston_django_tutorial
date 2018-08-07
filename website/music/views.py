1b7c49427411d3d3e601de86fab17bc828695105
Vaibhav Paliwal <vaibhav.paliwal@outlook.com>
2018-08-07 15:29:50 +0530

    - Implemented the 'detail' view and created the corresponding template

--- website/music/views.py
+++ website/music/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
+from django.http import Http404
 from .models import Album
 from django.shortcuts import render
 
+
 def index(request):
     all_albums = Album.objects.all()
+
     context = {
         'all_albums' : all_albums,
     }
@@ -12,4 +14,13 @@ def index(request):
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2> Details for album id: %d </h2>" % (album_id))
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist!")
+
+    context = {
+        'album' : album,
+    }
+
+    return render(request, 'music/detail.html', context=context)
