77b114ad496c0180ff39b418937722adf845d336
Vaibhav Paliwal <vaibhav.paliwal@outlook.com>
2018-08-07 14:03:04 +0530

    - Added a 'detail' view for looking at details of an album
    - Updated url routing accordingly

--- website/music/urls.py
+++ website/music/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    # ex: /music/
     path('', views.index, name='index'),
+    # ex: /music/71/
+    path('<int:album_id>/', views.detail, name='detail'),
 ]
--- website/music/views.py
+++ website/music/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello Buddy!!')
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2> Details for album id: %d </h2>" % (album_id))
