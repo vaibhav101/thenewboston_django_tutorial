4d58f2ff994af80c4033bbd7a07b8c48fdad9673
Vaibhav Paliwal <vaibhav.paliwal@outlook.com>
2018-08-08 14:45:42 +0530

    - used django.shortcuts.get_object_or_404 instead of using a try/except block - Corrected some minor PEP8 violations

--- website/music/views.py
+++ website/music/views.py
@@ -1,26 +1,22 @@
-from django.http import Http404
 from .models import Album
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     all_albums = Album.objects.all()
 
     context = {
-        'all_albums' : all_albums,
+        'all_albums': all_albums,
     }
 
     return render(request, 'music/index.html', context=context)
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist!")
+    album = get_object_or_404(Album, pk=album_id)
 
     context = {
-        'album' : album,
+        'album': album,
     }
 
     return render(request, 'music/detail.html', context=context)
